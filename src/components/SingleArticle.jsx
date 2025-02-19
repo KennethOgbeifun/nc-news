6c813923cc763cb0dc8aa7b5cb24093a76733e1d
Ed <simplifieducation@gmail.com>
2025-02-19 12:13:40 +0000

    completed basic single article page available at the parametic endpoint for articles

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,9 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export function SingleArticle() {
   const { article_id } = useParams();
   const [singleArticle, setSingleArticle] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  return <></>;
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://ncnews-clfh.onrender.com/api/articles/${article_id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSingleArticle(data);
+        console.log(data);
+
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="article-title">{singleArticle.title}</h1>
+        <p className="article-data">
+          By {singleArticle.author} on{" "}
+          {new Date(singleArticle.created_at).toLocaleDateString()}
+        </p>
+        {singleArticle.article_img_url && (
+          <img
+            className="article-img"
+            src={singleArticle.article_img_url}
+            alt={singleArticle.title}
+          />
+        )}
+        <p className="article-body">{singleArticle.body}</p>
+      </div>
+    </>
+  );
 }
